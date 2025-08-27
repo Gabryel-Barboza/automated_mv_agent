f80984a21eaa1c7b84c3d3442e916be714c8c43d
Gabryel_Brz <gabrielbarbosa.alternativa@gmail.com>
2025-08-27 05:41:44 -0300

    Add render blueprint, add cors to fastapi backend, add MySQL database to n8n flow

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,19 @@
 import io
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 
 from services import sheet_data_processing
 
 app = FastAPI(docs_url='', redoc_url='')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.get('/api/upload', status_code=200)
